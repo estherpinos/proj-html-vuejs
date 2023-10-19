ee2d26ca7e1f2966a3706d40b0a0a5cf334acf36
estherpinos <estherpinos1@gmail.com>
2023-10-19 17:02:30 +0200

    Add [structura jumbotron]

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,9 +6,29 @@ export default{
 </script>
 
 <template>
+  <main>
+    <section class="section1">
+      <div>
+        <div class="jumbotron">
+          <div class="text-jumbo"></div>
+          <div class="img-jumbo"></div>
+        </div>
+        <div class="arrow-left"></div>
+        <div class="arrow-right"></div>
+      </div>
+    </section>
+  </main>
 
 </template>
 
 <style lang="scss" scoped>
 @use '../sass/variabiles' as *;
+
+.section1{
+  .jumbotron{
+    height: 350px;
+    
+  }
+}
+
 </style>
